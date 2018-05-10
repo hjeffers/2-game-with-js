5670ed248169cf30d9acb9713016bed8bc6bbd1c
hjeffers <haleyjeffers@gmail.com>
2018-05-09 18:50:01 -0700

    First run with red points

--- display.js
+++ display.js
@@ -6,12 +6,17 @@ function setup(){
     var width = 400;
     var height = 400;
     game = createGameState();
-    frameRate(2);
+    frameRate(2); //calls 'draw' 2x per second
     createCanvas(width, height); 
 }
 
 function draw(){
+    fill("red");
     background(0);
+    drawPoint(0,0);
+    drawPoint(10,0);
+    drawPoint(0,10);
+    drawPoint(10,10);
 }
 
 var pointSize = 10;
@@ -23,7 +28,9 @@ var colors = [
     "#fec107"
 ];
 
+//converts logical grid into physical display
 function drawPoint(x,y,colorIndex){
+    ellipse(x * pointSpacing, y * pointSpacing,  pointSize)
     console.log(`drawPoint: ${keyCode}`);
 }
 
--- game.js
+++ game.js
@@ -1,6 +1,28 @@
 // functions
 
-function createGameState(){}
+function createGameState(){
+    var game = {};
+    game.playerX = 5;
+    game.playerY = 10;
+
+
+    var grid = [];
+    grid.push([1,1,1,1,1])
+    grid.push([1,0,0,0,1])
+    grid.push([1,0,0,0,1])
+    grid.push([1,0,0,0,1])
+    grid.push([1,0,0,0,1])
+    grid.push([1,0,0,0,1])
+    grid.push([1,1,1,1,1])
+
+    game.grid = grid;
+
+    return game;
+}
+
+//To get certain coordinate, have to specify which row/array and then which element in that row (instead of x,y)
+//Remember to count from 0!
+
 function createEmptyGrid(){}
 function createLevel(){}
 function setRandomCoin(){}
